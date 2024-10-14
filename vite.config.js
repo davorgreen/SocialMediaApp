8fea2be2a6e2bb731039934e4d3a7e34c62102d0
Davor Grinfelder <grinfelderdavor@gmail.com>
2024-10-14 21:42:08 +0200

    vire config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
-      '/api': 'https://green-api-nu.vercel.app',
+      '/api': {
+        target: 'https://green-api-nu.vercel.app/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
-  plugins: [react()],
 });
